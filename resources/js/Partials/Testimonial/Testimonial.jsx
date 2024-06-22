44858c6697aa7a319541176836e77390b2bfa20b
Utibe-Abasi Emmanuel <utyemma@gmail.com>
2024-06-22 11:49:36 +0100

    Handled Testimonials

--- resources/js/Partials/Testimonial/Testimonial.jsx
+++ resources/js/Partials/Testimonial/Testimonial.jsx
@@ -1,59 +1,167 @@
-import React, { useState } from 'react'
+import React, { useEffect, useMemo, useState } from 'react'
 import TestimonialContent from './TestimonialContent';
 import TestimonialItem from './TestimonialItem';
 import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/24/solid';
+import { Transition } from '@headlessui/react';
+import clsx from 'clsx';
 
 export default function () {
 
+    const [items, setItems] = useState([
+        {
+            message: 'I had a top-notch experience with Healthbubba! The speed and affordability of their services are unmatched.',
+            image: '/assets/imgs/avatars/jane-nwosu.png',
+            role: 'Healthcare Professional',
+            author: 'Jane Nwosu'
+        },
+        {
+            message: 'I had a top-notch experience with Healthbubba! The speed and affordability of their services are unmatched.',
+            image: '/assets/imgs/avatars/jane-nwosu.png',
+            role: 'Healthcare Professional',
+            author: 'Jane Nwosu'
+        },
+        {
+            message: 'I had a top-notch experience with Healthbubba! The speed and affordability of their services are unmatched.',
+            image: '/assets/imgs/avatars/jane-nwosu.png',
+            role: 'Healthcare Professional',
+            author: 'Jane Nwosu'
+        },
+        {
+            message: 'I had a top-notch experience with Healthbubba! The speed and affordability of their services are unmatched.',
+            image: '/assets/imgs/avatars/jane-nwosu.png',
+            role: 'Healthcare Professional',
+            author: 'Jane Nwosu'
+        },
+    ])
+
+
     const [active, setActive] = useState(0);
     const [next, setNext] = useState(1);
-    const [prev, setPrev] = useState(2);
+    const [prev, setPrev] = useState(items.length - 1);
+
+    // items.length - 1
+
+    const [forward, setForward] = useState(false)
+
+    const isLastItem = useMemo(() => next == items.length, [next])
+
+    const nextItem = () => {
+        if(next == items.length) {
+            setActive(0)
+            setNext(1)
+            setPrev(items.length - 1)
+            // items.length - 1
+            return
+        };
+
+        setForward(true)
+        setActive(active + 1);
+        setNext(next + 1);
+        setPrev(prev => {
+            let num = prev + 1;
+            return num >= items.length ? 0 : num;
+        });
+    }
+    
+    const prevItem = () => {
+        if(prev == null) return;
+
+        setForward(false)
+
+        setActive(active - 1);
+        setNext(next - 1);
+        setPrev(prev => {
+            let num = prev - 1
+            return num <= 0 ? items.length - 1 : num 
+        });
+
+    }
 
 
 
     return (
         <div className="flex flex-col md:flex-row md:space-x-20 justify-center items-center px-4">
             <div>
-                <button className="hidden md:block rounded-full p-3 bg-white shadow border">
+                <button onClick={prevItem} className="hidden md:block rounded-full p-3 bg-white shadow border">
                     <ChevronLeftIcon className="w-5 h-5" />
                 </button>
             </div>
 
             <div className="md:w-4/6 relative">
-                <div className="px-5 md:px-10">
-                    <div className='flex space-x-2  md:space-x-5 absolute items-center top-0 bottom-0 z-0 right-0 left-0'>
-                        <TestimonialItem
-                            message="I had a top-notch experience with Healthbubba! The speed and affordability of their services are unmatched. "
-                            image={'/assets/imgs/avatars/jane-nwosu.png'}
-                            author={'Jane Nwosu'}
-                            role={'Healthcare Professional'}
-                            classes={{
-                                message: 'line-clamp-1'
-                            }}
-                        />
-                        <TestimonialItem
-                            message="I had a top-notch experience with Healthbubba! The speed and affordability of their services are unmatched. "
-                            image={'/assets/imgs/avatars/jane-nwosu.png'}
-                            author={'Jane Nwosu'}
-                            role={'Healthcare Professional'}
-                            classes={{
-                                message: 'line-clamp-1'
-                            }}
-                        />
+                <div className="px-5 md:px-0 relative p-5 overflow-hidden">
+                    <div className={'flex items-center p-10 px-5 space-x-3 ' + `${isLastItem ? 'justify-start' : 'justify-end'}`}>
+                        {
+                            items.map((item, index) => (
+                                <>
+                                    <Transition as='div' 
+                                        className={'w-1/2 duration-1000'} 
+                                        show={index == prev}  
+                                        enterFrom='translate-x-full'
+                                        enterTo='translate-x-0'
+                                        leaveFrom='translate-x-0'
+                                        leaveTo='scale-110 -translate-x-40'
+                                        leave='absolute z-50 hidden'
+                                    >
+                                        <TestimonialItem 
+                                            {...item}
+                                            classes={{
+                                                message: 'line-clamp-1'
+                                            }}
+                                            index={index}
+                                            next={next}
+                                            prev={prev}
+                                            label={index == prev ? 'prev' : 'next'}
+                                            active={false}
+                                        />
+                                    </Transition>
+
+                                    <Transition as='div' className={'w-1/2 duration-1000'} 
+                                        enterFrom='translate-x-full'
+                                        enterTo='translate-x-0'
+                                        leaveFrom='translate-x-0'
+                                        leaveTo='scale-110 -translate-x-40'
+                                        leave='absolute z-50 hidden'
+                                        show={index == next}  >
+                                        <TestimonialItem 
+                                            {...item}
+                                            classes={{
+                                                message: 'line-clamp-1'
+                                            }}
+                                            index={index}
+                                            next={next}
+                                            prev={prev}
+                                            label={index == next ? 'next' : 'prev'}
+                                            active={false}
+                                        />
+                                    </Transition>
+                                
+                                    <Transition 
+                                        show={index == active} 
+                                        as='div'  
+                                        className={`absolute left-16 right-16 z-10 duration-1000 py-5`}
+                                        enterFrom={`${forward ? 'translate-x-full' : '-translate-x-full'} scale-0`}
+                                        enterTo='scale-100 translate-0'
+                                        leaveFrom='scale-100 translate-0'
+                                        leaveTo={`top-0 bottom-0 left-0 right-0 scale-0 ${forward ? '-translate-x-full' : ' translate-x-full'}`}
+                                    >
+                                        <TestimonialItem
+                                            message="I had a top-notch experience with Healthbubba! The speed and affordability of their services are unmatched. "
+                                            image={'/assets/imgs/avatars/jane-nwosu.png'}
+                                            author={'Jane Nwosu'}
+                                            role={'Healthcare Professional'}
+                                            active={index == active}
+                                        />
+                                    </Transition>
+                                </>
+                            ))
+                        }
                     </div>
 
-                    <TestimonialItem
-                        message="I had a top-notch experience with Healthbubba! The speed and affordability of their services are unmatched. "
-                        image={'/assets/imgs/avatars/jane-nwosu.png'}
-                        author={'Jane Nwosu'}
-                        role={'Healthcare Professional'}
-                        active={true}
-                    />
                 </div>
             </div>
 
             <div>
-                <button className="hidden md:block rounded-full p-3 bg-white shadow border">
+                <button onClick={nextItem} className="hidden md:block rounded-full p-3 bg-white shadow border">
                     <ChevronRightIcon className="w-5 h-5" />
                 </button>
             </div>
@@ -69,3 +177,38 @@ export default function () {
         </div>
     )
 }
+
+const Items = () => (
+    <div className="md:w-4/6 relative">
+        <div className="px-5 md:px-10">
+            <div className='flex space-x-2  md:space-x-5 absolute items-center top-0 bottom-0 z-0 right-0 left-0'>
+                <TestimonialItem
+                    message="I had a top-notch experience with Healthbubba! The speed and affordability of their services are unmatched."
+                    image={'/assets/imgs/avatars/jane-nwosu.png'}
+                    author={'Jane Nwosu'}
+                    role={'Healthcare Professional'}
+                    classes={{
+                        message: 'line-clamp-1'
+                    }}
+                />
+                <TestimonialItem
+                    message="I had a top-notch experience with Healthbubba! The speed and affordability of their services are unmatched. "
+                    image={'/assets/imgs/avatars/jane-nwosu.png'}
+                    author={'Jane Nwosu'}
+                    role={'Healthcare Professional'}
+                    classes={{
+                        message: 'line-clamp-1'
+                    }}
+                />
+            </div>
+
+            <TestimonialItem
+                message="I had a top-notch experience with Healthbubba! The speed and affordability of their services are unmatched. "
+                image={'/assets/imgs/avatars/jane-nwosu.png'}
+                author={'Jane Nwosu'}
+                role={'Healthcare Professional'}
+                active={true}
+            />
+        </div>
+    </div>
+)
--- resources/js/Partials/Testimonial/TestimonialItem.jsx
+++ resources/js/Partials/Testimonial/TestimonialItem.jsx
@@ -1,24 +1,26 @@
 import React from 'react'
 import TestimonialContent from './TestimonialContent'
 
-export default function ({active = false, message, author, role, image, classes = {
+export default function ({active = false, message, author,index, next, prev, label,  role, image, classes = {
     message: '',
     image: '',
     author: '',
     role: ''
 }, className, ...props}) {
     return (
-        <div style={{
-            zoom: active ? 1 : 0.95
-        }} className={`${active ? '' : 'bg-white shadow-lg p-5 md:p-10 md:rounded-2xl rounded-lg'}  overflow-hidden ${className}`}>
-            <div className={active ? 'md:rounded-2xl rounded-lg bg-white bg-opacity-95 shadow-lg p-5 md:p-16 space-y-5 relative z-50' : 'blur'}>
-                <div className={`space-y-5`}>
-                    <p className={`text-muted text-sm md:text-lg font-medium leading-relaxed ${classes.message}`}>"{message}"</p>
-                    <div className="justify-start items-center gap-4 inline-flex">
-                        <img className={`h-14 ${classes.image}`} src={image} />
-                        <div className="flex-col text-xs md:text-base justify-start items-start inline-flex">
-                            <div className={`text-neutral-900 font-medium ${author}`}>{author}</div>
-                            <div className={`text-gray-600 text-base font-normal ${role}`}>{role}</div>
+        <div className='relative'>
+            <div style={{
+                zoom: active ? 1 : 0.95
+            }} className={`${active ? '' : 'bg-white p-5 md:p-10 md:rounded-2xl rounded-lg'}  overflow-hidden ${className}`}>
+                <div className={active ? 'md:rounded-2xl rounded-lg bg-white bg-opacity-95 p-5 md:p-16 space-y-5 relative z-50' : 'blur-sm'}>
+                    <div className={`space-y-5`}>
+                        <p className={`text-muted text-sm md:text-lg font-medium leading-relaxed ${classes.message}`}>"{message}"</p>
+                        <div className="justify-start items-center gap-4 inline-flex">
+                            <img className={`h-14 ${classes.image}`} src={image} />
+                            <div className="flex-col text-xs md:text-base justify-start items-start inline-flex">
+                                <div className={`text-neutral-900 font-medium ${author}`}>{author}</div>
+                                <div className={`text-gray-600 text-base font-normal ${role}`}>{role}</div>
+                            </div>
                         </div>
                     </div>
                 </div>
