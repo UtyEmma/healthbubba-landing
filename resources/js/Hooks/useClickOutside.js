b422627fa4b65ec7d6216cb626e19e284a70be01
Utibe-Abasi Emmanuel <utyemma@gmail.com>
2024-11-16 20:03:18 +0100

    Use Click outside

---
+++ resources/js/Hooks/useClickOutside.js
@@ -0,0 +1,19 @@
+import { useEffect } from "react";
+
+function useClickOutside(ref, handler) {
+  useEffect(() => {
+    function handleClickOutside(event) {
+      if (ref.current && !ref.current.contains(event.target)) {
+        handler(event);
+      }
+    }
+
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [ref, handler]);
+}
+
+export default useClickOutside;
