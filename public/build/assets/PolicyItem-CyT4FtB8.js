830c13b544afe97ceceb459cf5b3c5dda3515314
Utibe-Abasi Emmanuel <utyemma@gmail.com>
2024-11-17 10:00:13 +0100

    Fix for the route

--- public/build/assets/PolicyItem-CyT4FtB8.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./app-BPkFujP4.js";const x=({title:e,children:s})=>t.jsxs("div",{class:"md:py-5 py-2 space-y-3",children:[t.jsx("h2",{class:"text-xl md:text-2xl font-medium",children:e}),t.jsx("div",{class:"bg-gray-100 text-sm md:text-base p-4 md:p-5 space-y-3 border rounded-lg text-muted",children:s})]});export{x as PolicyItem};
