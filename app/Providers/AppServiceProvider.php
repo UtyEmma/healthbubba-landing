ed288953b8a86118513293d7dabf500582a40124
Utibe-Abasi Emmanuel <utyemma@gmail.com>
2026-03-07 12:13:37 +0100

    asas

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -12,14 +13,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
-    }
-
-    /**
-     * Bootstrap any application services.
-     */
+        }
+        
+        /**
+         * Bootstrap any application services.
+        */
     public function boot(): void
     {
+        Schema::defaultStringLength(191);
         JsonResource::withoutWrapping();
     }
 }
