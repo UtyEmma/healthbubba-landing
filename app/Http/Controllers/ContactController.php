5474df1ad8f1e4b558671322c196fa971815e705
Utibe-Abasi Emmanuel <utyemma@gmail.com>
2026-05-28 07:38:59 +0100

    updated the contact

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -57,17 +57,17 @@ class ContactController extends Controller
             //throw $th;
         }
 
-    notify("Thank You for Reaching Out, {$request->name}!")
-        ->greeting("Hello {$request->name},")
-        ->line('Thank you for contacting Healthbubba. We have received your message and our team will review it shortly.')
-        ->line('Here’s a summary of your submission:')
-        ->line("**Name:** {$request->name}")
-        ->line("**Email Address:** {$request->email}")
-        ->line("**Phone Number:** {$request->phone}")
-        ->line("**Message:** {$request->message}")
-        ->line('Our team will get back to you within the next 24-48 hours. If your matter is urgent, feel free to contact us directly.')
-        ->salutation('Best regards, The Healthbubba Team')
-        ->mail($request->email);
+    // notify("Thank You for Reaching Out, {$request->name}!")
+    //     ->greeting("Hello {$request->name},")
+    //     ->line('Thank you for contacting Healthbubba. We have received your message and our team will review it shortly.')
+    //     ->line('Here’s a summary of your submission:')
+    //     ->line("**Name:** {$request->name}")
+    //     ->line("**Email Address:** {$request->email}")
+    //     ->line("**Phone Number:** {$request->phone}")
+    //     ->line("**Message:** {$request->message}")
+    //     ->line('Our team will get back to you within the next 24-48 hours. If your matter is urgent, feel free to contact us directly.')
+    //     ->salutation('Best regards, The Healthbubba Team')
+    //     ->mail($request->email);
 
     
 
