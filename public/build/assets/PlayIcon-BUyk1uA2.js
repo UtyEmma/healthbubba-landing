b0001e1b63ca5e2260cd4cc9be56096c1318b3f0
Utibe-Abasi Emmanuel <utyemma@gmail.com>
2026-08-25 14:45:42 +0100

    Updated the Whatsapp button

---
+++ public/build/assets/PlayIcon-BUyk1uA2.js
@@ -0,0 +1,1 @@
+import{j as o}from"./app-DcT6TKSj.js";import{F as t}from"./CartModal-DflN9dSb.js";function m({...r}){return o.jsx(t,{...r})}export{m as P};
--- public/build/assets/PlayIcon-D_2UWLBc.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./app-BA6RLZq_.js";import{F as t}from"./CartModal-D5oi80bW.js";function m({...r}){return o.jsx(t,{...r})}export{m as P};
