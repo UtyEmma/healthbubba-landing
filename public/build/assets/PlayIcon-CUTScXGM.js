736a4807f1410ac262e42f3ec44f0c0ddfe554e0
Utibe-Abasi Emmanuel <utyemma@gmail.com>
2026-09-09 13:27:20 +0100

    update the buttone

--- public/build/assets/PlayIcon-CUTScXGM.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./app-B5IkA-rt.js";import{F as t}from"./CartModal-UagKGabo.js";function m({...r}){return o.jsx(t,{...r})}export{m as P};
---
+++ public/build/assets/PlayIcon-ClXSKvFP.js
@@ -0,0 +1,1 @@
+import{j as o}from"./app-l02ECKEQ.js";import{F as t}from"./CartModal-Dc1dQMLb.js";function m({...r}){return o.jsx(t,{...r})}export{m as P};
